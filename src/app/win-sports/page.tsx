4b469b7a0d8ff198c626a5b8aca9f6706d8e536b
assadakram <assad5724@gmail.com>
2024-08-19 22:30:59 +0500

    minor change

--- src/app/win-sports/page.tsx
+++ src/app/win-sports/page.tsx
@@ -2,7 +2,7 @@ export default function Index() {
 
     return (
         <div>
-            <embed src="https://win-esports.com/" className={"w-[100%] h-[100vh]"} frameborder={"0"}/>
+            <iframe src="https://win-esports.com/" className="w-[100%] h-[100vh]" />
         </div>
     );
 }

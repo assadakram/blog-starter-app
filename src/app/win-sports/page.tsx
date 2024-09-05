b687572fba7ae74ffe59a56982342c909a49823c
Bf2rr <105173099+Bf2rr@users.noreply.github.com>
2024-09-05 19:09:51 +0200

    Update page.tsx

--- src/app/win-sports/page.tsx
+++ src/app/win-sports/page.tsx
@@ -2,7 +2,7 @@ export default function Index() {
 
     return (
         <div>
-            <iframe src="https://win-esports.com/" className="w-[100%] h-[100vh]" />
+            <iframe src="https://esports-win.com/" className="w-[100%] h-[100vh]" />
         </div>
     );
 }
